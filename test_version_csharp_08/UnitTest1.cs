b6373b96d24eb15976c061993aa36f3b28905515
RalfKonrad <github@eckel-consulting.de>
2025-01-02 20:16:40 +0100

    Add test_version_csharp_08 project

---
+++ test_version_csharp_08/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace test_version_csharp_08;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
